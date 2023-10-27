c0141c87f7e5fbdc9c34ee67bf7a4655de0a371e
Gary Ridgway <gary-ridgway@uiowa.edu>
2023-10-27 15:42:13 -0500

    Lots of cool things like scan and leaves.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,19 @@
 // Get the checkbox
 let fpCheckBox;
 let eCheckBox;
+let kCheckBox;
+let lCheckBox;
 let zoomSlide;
 let lancetWindow;
 // Get the output text
 let body;
 let personSlider;
 let person;
+let kinect;
+let leaves;
 let lines;
+let scanTo;
+let scanFrom;
 
 let layer1Tex;
 let layer2Tex;
@@ -20,13 +26,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Get the checkbox
     fpCheckBox = document.getElementById("firstPerson");
     eCheckBox = document.getElementById("expand");
+    kCheckBox = document.getElementById("scan");
+    lCheckBox = document.getElementById("leaves");
+
     zoomSlide = document.getElementById("zoom");
     lancetWindow = document.getElementById("window");
     // Get the output text
     body = document.querySelector("body");
     personSlider = document.getElementById("personPosition");
     person = document.getElementById("person");
+    kinect = document.getElementById("kinect");
+    leaves = document.getElementById("layer1-1");
     lines = document.querySelectorAll(".viewline .texture");
+
+    scanTo = document.querySelectorAll(".kinect-scan.kinect-scan-to");
+    scanFrom = document.querySelectorAll(".kinect-scan.kinect-scan-from");
 
     layer1Tex = document.querySelector("#layer1 .texture");
     layer2Tex = document.querySelector("#layer2 .texture");
@@ -35,13 +49,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     layer5Tex = document.querySelector("#layer5 .texture");
 
     initMovement();
-
+    preCheck();
     goFirstPerson();
 });
 
 function goFirstPerson() {
     // If the checkbox is checked, display the output text
-    triggerFPAnimation();
+    makeTransitory(body, 700);
     if (fpCheckBox.checked == true) {
         body.classList.add("first");
     } else {
@@ -49,11 +63,12 @@ function goFirstPerson() {
     }
 }
 
-function triggerFPAnimation() {
-    body.classList.add("transitory");
-    asyncDelay(700, function () {
-        body.classList.remove("transitory");
-    });
+function makeTransitory(element, time) {
+  console.log(element);
+  element.classList.add("transitory");
+  asyncDelay(time, function () {
+    element.classList.remove("transitory");
+  });
 }
 
 function slidePerson() {
@@ -65,7 +80,10 @@ function slidePerson() {
     const angle = -(calcAngleDegrees(pos, -354) + 90);
     lines.forEach(function (line) {
       body.style.setProperty("--angle", angle + "deg");
-      // line.style.transform = "rotate3d(0, 0, 1, " + angle + "deg)";
+      const scanAngle = scale(angle, 24.724, -24.724, -19.5, 19.5);
+      body.style.setProperty("--scan-angle", scanAngle + "deg");
+      const scanSlide = scale(angle, 24.724, -24.724, -24, 24);
+      body.style.setProperty("--scan-slide", scanSlide + "px");
     });
   
     // const doDamp = parseInt(
@@ -76,7 +94,7 @@ function slidePerson() {
     // Above not working yet.
     const doDamp = 1;
   
-    const x1Damp = doDamp > 0 ? 0.35 : 1;
+    const x1Damp = doDamp > 0 ? 0.5 : 1;
     const x1 = calcPlanePos(100, angle + 90) / x1Damp;
     layer1Tex.style.setProperty("--lateral", -x1 + "px");
   
@@ -97,31 +115,67 @@ function slidePerson() {
     layer5Tex.style.setProperty("--lateral", -x5 + "px");
   }
 
-  function calcAngleDegrees(x, y) {
-    return (Math.atan2(y, x) * 180) / Math.PI;
-  }
+function calcAngleDegrees(x, y) {
+  return (Math.atan2(y, x) * 180) / Math.PI;
+}
 
-  function forceExpand() {
-    // If the checkbox is checked, display the output text
-    if (eCheckBox.checked == true) {
-      lancetWindow.classList.add("force-expand");
-    } else {
-      lancetWindow.classList.remove("force-expand");
-    }
+function forceExpand() {
+  // If the checkbox is checked, display the output text
+  if (eCheckBox.checked == true) {
+    lancetWindow.classList.add("force-expand");
+    eCheckBox.parentElement.classList.add("checked");
+    makeTransitory(lancetWindow, 650)
+  } else {
+    lancetWindow.classList.remove("force-expand");
+    eCheckBox.parentElement.classList.remove("checked");
+    makeTransitory(lancetWindow, 350)
   }
+}
 
-  function calcPlanePos(y, angle) {
-    const rads = (angle / 180) * Math.PI;
-    const tanang = y / Math.tan(-rads);
-  
-    return tanang;
-  }
+function calcPlanePos(y, angle) {
+  const rads = (angle / 180) * Math.PI;
+  const tanang = y / Math.tan(-rads);
 
-  function hypot(a, b) {
-    console.log(a, b);
-    const leftSum = Math.pow(a, 2) + Math.pow(b, 2);
-    console.log(leftSum);
-    const c = Math.sqrt(leftSum);
-    console.log(c);
-    return c;
+  return tanang;
+}
+
+function hypot(a, b) {
+  console.log(a, b);
+  const leftSum = Math.pow(a, 2) + Math.pow(b, 2);
+  console.log(leftSum);
+  const c = Math.sqrt(leftSum);
+  console.log(c);
+  return c;
+}
+
+function kinectTracking() {
+  // If the checkbox is checked, display the output text
+  if (kCheckBox.checked == true) {
+    kinect.classList.add("show-scan");
+    kCheckBox.parentElement.classList.add("checked");
+    makeTransitory(lancetWindow, 650)
+  } else {
+    kinect.classList.remove("show-scan");
+    kCheckBox.parentElement.classList.remove("checked");
+    makeTransitory(lancetWindow, 350)
   }
+}
+
+function displayLeaves() {
+  // If the checkbox is checked, display the output text
+  if (lCheckBox.checked == true) {
+    leaves.classList.add("show-leaves");
+    lCheckBox.parentElement.classList.add("checked");
+    makeTransitory(leaves, 650)
+  } else {
+    leaves.classList.remove("show-leaves");
+    lCheckBox.parentElement.classList.remove("checked");
+    makeTransitory(leaves, 350)
+  }
+}
+
+function preCheck() {
+  forceExpand();
+  kinectTracking();
+  displayLeaves();
+}
